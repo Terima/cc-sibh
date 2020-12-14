e352b4b18bdbe6efe0b0a85c82aa1f0df8759bd6
Terima <tralop15@gmail.com>
2020-12-14 16:51:04 +0800

    Menambahkan koneksi database ke remotemysql

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,20 @@
 <?php 
-try { 
-$pdo_conn = new PDO('mysql:host=localhost;dbname=sibh', 'root', '',
-array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,PDO::ATTR_PERSISTENT => true)); 
-} 
-catch(PDOException $e) { 
-print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>"; 
-die();
-} 
+
+    $host = 'remotemysql.com';
+    $db = '3nVCoE0ncB';
+    $user = '3nVCoE0ncB';
+    $pass = 'q20fhxTvLg';
+    $charset ='utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+    try { 
+        $pdo_conn = new PDO($dsn, $user, $pass);
+        array(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION,PDO::ATTR_PERSISTENT => true)); 
+    } 
+
+    catch(PDOException $e){ 
+        print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>"; 
+        die();
+    } 
 ?>
