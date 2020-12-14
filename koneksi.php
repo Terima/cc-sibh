a4481cb73768a61f0249a14eb116e42f5cdad7a3
Terima <tralop15@gmail.com>
2020-12-14 17:27:23 +0800

    Update koneksi.php
    akhirnya tidak error, tadi salah nama variable

--- koneksi.php
+++ koneksi.php
@@ -8,7 +8,7 @@
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
     try { 
-        $pdo_conn = new PDO($dsn, $user, $pass);
+        $pdo = new PDO($dsn, $user, $pass);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } 
 
